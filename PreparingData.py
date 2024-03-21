a0689b7bd138764e0896542b83356bc09867ff72
Your Name <you@example.com>
2024-03-21 19:56:30 -0400

    how are you? and I am good phrases

---
+++ PreparingData.py
@@ -0,0 +1,27 @@
+import sounddevice as sd
+from scipy.io.wavfile import write
+
+def record_audio_and_save(save_path, n_times):
+    """
+    This function will run `n_times` and everytime you press Enter you have to speak the wake word
+
+    Parameters
+    ----------
+    n_times: int, default=50
+        The function will run n_times default is set to 50.
+
+    save_path: str
+        Where to save the wav file which is generated in every iteration.
+    """
+
+    input("To start recording Wake Word press Enter: ")
+    for i in range(n_times):
+        fs = 16000
+        seconds = 2
+
+        myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
+        sd.wait()
+        write(save_path + "/s" + str(i+94) + ".wav", fs, myrecording)
+        input(f"Press to record next or two stop press ctrl + C ({i + 1}/{n_times}): ")
+
+record_audio_and_save("mam_phrases/p2",8)
